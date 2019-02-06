a09acf70a7ba5b249ea18bc9b32e64572d10cbf9
ray2019 <ren.rui@udacity.com>
2019-02-06 16:32:29 +0800

    feat(#query): Query the formation

--- SQLFormat.py
+++ SQLFormat.py
@@ -7,12 +7,36 @@
 
 
 from pandas import DataFrame
-import sqlite3
+from sqlite3 import Connection
 
-class SQLFormat(DataFrame):
+class SQLFormat(Connection):
     def __init__(self, connection):
         """Init object
         connection: SQLite connection
         """
-        super().__init__()
+        super().__init__
+        self.connection = connection
         self.cursor = connection.cursor()
+    
+    def query(self, query_string):
+        """Query Information
+        Query the database, and get the column names
+
+        Parameters:
+        ------------
+        query_string: string
+            SQL query string
+        """
+        self.connection.text_factory = str
+
+        try:
+            result = self.cursor.execute(query_string).fetchall()
+        except:
+            self.connection.text_factory = bytes
+            result = self.cursor.execute(query_string).fetchall()
+        
+        columns = []
+        for column in self.cursor.description:
+            columns.append(column[0])
+        
+        return result, columns
